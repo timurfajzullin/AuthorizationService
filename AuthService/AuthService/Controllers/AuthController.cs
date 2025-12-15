2171a943baf8c2fdd3b891bc2f5a24c9219c0ba5
FayzullinTR <timur.faizullin@social-card.ru>
2025-12-15 11:42:23 +0300

    init

---
+++ AuthService/AuthService/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class AuthController : ControllerBase
+{
+    [HttpGet]
+    public async Task<string> GetAccessToken()
+    {
+        
+    }
+}
